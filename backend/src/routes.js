f1d1ca3fadf564b694c63c3bac133d92c2102d3c
Stefano Saffran <stefanosaffran@hotmail.com>
2020-01-14 21:41:45 +0100

    feat: add api routes

---
+++ backend/src/routes.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
+
+const routes = Router();
+
+routes.post('/devs', DevController.store);
+routes.get('/devs', DevController.index);
+routes.put('/devs/:id', DevController.update);
+routes.delete('/devs/:id', DevController.delete);
+
+routes.get('/search', SearchController.index);
+
+
+module.exports = routes;
