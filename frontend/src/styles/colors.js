168225794b65e7d5ce52c513cf9bb317d6f49457
Stefano Saffran <stefanosaffran@hotmail.com>
2020-01-16 12:13:01 +0100

    feat: add app styles

---
+++ frontend/src/styles/colors.js
@@ -0,0 +1,13 @@
+export default {
+  primary: '#61dbfb',
+  supporting: '#e5e6f0',
+  submitButton: '#4d85ee',
+  backButton: '#ccc',
+  white: '#fff',
+  border: '#ddd',
+  lightBorder: '#eee',
+  lightGray: '#999',
+  gray: '#666',
+  darkGray: '#333',
+  label: '#acacac',
+};
---
+++ frontend/src/styles/global.js
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from 'styled-components';
+
+import { darken } from 'polished';
+import colors from '~/styles/colors';
+
+import 'react-toastify/dist/ReactToastify.css';
+import 'react-activity/dist/react-activity.css';
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+* {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+
+  body {
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+    background: linear-gradient(
+    45deg,
+    ${colors.primary},
+    ${darken(0.5, `${colors.primary}`)}
+  );
+  }
+
+  body, input, textarea, button, input::placeholder, textarea::placeholder {
+    font: 14px 'Roboto', sans-serif;;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
